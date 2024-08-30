d3b2e078d4d9cd6fe5a61f45f6e9fae7c82f862e
Mike Fridman <mfridman@buf.build>
2024-08-30 13:40:36 -0400

    update

--- README.md
+++ README.md
@@ -9,19 +9,32 @@ https://github.com/npm/registry/blob/main/docs/responses/package-metadata.md#abb
 Using a descently-sized JSON file as an input.
 
 ```shell
-go test -bench=. -benchmem -count=1 -v -benchtime=2s
+benchstat old.txt new.txt
 goos: darwin
 goarch: arm64
 pkg: github.com/mfridman/benchmark-go-json
 cpu: Apple M1 Max
-BenchmarkStandardLibrary
-BenchmarkStandardLibrary-10                   39          60079762 ns/op        18696526 B/op     554654 allocs/op
-BenchmarkStandardLibraryV2
-BenchmarkStandardLibraryV2-10                 36          58147962 ns/op         9785114 B/op     183463 allocs/op
-BenchmarkGoccyJSON
-BenchmarkGoccyJSON-10                         46          51471562 ns/op        32189440 B/op      30585 allocs/op
-BenchmarkSegmentJSON
-BenchmarkSegmentJSON-10                       66          36569542 ns/op         7373434 B/op     262043 allocs/op
-PASS
-ok      github.com/mfridman/benchmark-go-json   14.512s
+                     │   old.txt   │                new.txt                │
+                     │   sec/op    │    sec/op     vs base                 │
+StandardLibrary-10     58.94m ± 1%   128.71m ± 4%  +118.38% (p=0.000 n=10)
+StandardLibraryV2-10   57.75m ± 3%    81.42m ± 3%   +40.99% (p=0.000 n=10)
+GoccyJSON-10           50.83m ± 8%    68.55m ± 2%   +34.85% (p=0.000 n=10)
+SegmentJSON-10         36.46m ± 0%    88.36m ± 0%  +142.32% (p=0.000 n=10)
+geomean                50.12m         89.26m        +78.10%
+
+                     │    old.txt    │                  new.txt                   │
+                     │     B/op      │       B/op        vs base                  │
+StandardLibrary-10     21.03Mi ± 15%     15.24Mi ± 157%          ~ (p=0.137 n=10)
+StandardLibraryV2-10   9.332Mi ±  0%   238.565Mi ±   2%  +2456.42% (p=0.000 n=10)
+GoccyJSON-10           24.72Mi ± 56%     28.70Mi ±  79%          ~ (p=0.894 n=10)
+SegmentJSON-10         9.531Mi ±  2%     9.772Mi ±  80%          ~ (p=0.959 n=10)
+geomean                14.66Mi           31.78Mi          +116.72%
+
+                     │     old.txt     │                new.txt                 │
+                     │    allocs/op    │   allocs/op    vs base                 │
+StandardLibrary-10     554656.000 ± 0%    4.000 ± 250%  -100.00% (p=0.000 n=10)
+StandardLibraryV2-10      183.46k ± 0%   41.19k ±   0%   -77.55% (p=0.000 n=10)
+GoccyJSON-10            30583.000 ± 0%    8.000 ±  88%   -99.97% (p=0.000 n=10)
+SegmentJSON-10         262044.000 ± 0%    2.000 ±  50%  -100.00% (p=0.000 n=10)
+geomean                    169.0k         40.30          -99.98%
 ```
--- benchmarkgojson.go
+++ benchmarkgojson.go
@@ -1,5 +1,16 @@
 package benchmarkgojson
 
+import (
+	"bytes"
+	"sync"
+)
+
+var bufferPool = sync.Pool{
+	New: func() interface{} {
+		return new(bytes.Buffer)
+	},
+}
+
 // https://github.com/npm/registry/blob/main/docs/responses/package-metadata.md#abbreviated-metadata-format
 
 type AbbreviatedMetadata struct {
@@ -9,82 +20,167 @@ type AbbreviatedMetadata struct {
 	DistTags map[string]string         `json:"dist-tags"`
 }
 
-// func (o *AbbreviatedMetadata) MarshalJSON() ([]byte, error) {
-// 	b := bytes.NewBuffer(nil)
-
-// 	// IMPLEMENT ME
-
-// 	return b.Bytes(), nil
-// }
-
 type VersionObject struct {
-	Name    string      `json:"name"`
-	Version string      `json:"version"`
-	Dist    *DistObject `json:"dist"`
-
+	Name             string            `json:"name"`
+	Version          string            `json:"version"`
 	Deprecated       string            `json:"deprecated,omitempty"`
+	Dist             *DistObject       `json:"dist"`
 	Dependencies     map[string]string `json:"dependencies,omitempty"`
 	DevDependencies  map[string]string `json:"devDependencies,omitempty"`
 	PeerDependencies map[string]string `json:"peerDependencies,omitempty"`
 }
-
-// func (o *VersionObject) MarshalJSON() ([]byte, error) {
-// 	b := bytes.NewBuffer(nil)
-// 	b.WriteString(`{"name":"`)
-// 	b.WriteString(o.Name)
-// 	b.WriteString(`","version":"`)
-// 	b.WriteString(o.Version)
-// 	b.WriteString(`","dist":`)
-// 	by, err := o.Dist.MarshalJSON()
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	b.Write(by)
-// 	if o.Deprecated != "" {
-// 		b.WriteString(`,"deprecated":"`)
-// 		b.WriteString(o.Deprecated)
-// 		b.WriteString(`"`)
-// 	}
-// 	if len(o.Dependencies) > 0 {
-// 		b.WriteString(`,"dependencies":`)
-// 		by, err := json.Marshal(o.Dependencies)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		b.Write(by)
-// 	}
-// 	if len(o.DevDependencies) > 0 {
-// 		b.WriteString(`,"devDependencies":`)
-// 		by, err := json.Marshal(o.DevDependencies)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		b.Write(by)
-// 	}
-// 	if len(o.PeerDependencies) > 0 {
-// 		b.WriteString(`,"peerDependencies":`)
-// 		by, err := json.Marshal(o.PeerDependencies)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		b.Write(by)
-// 	}
-// 	b.WriteString("}")
-// 	return b.Bytes(), nil
-// }
 
 type DistObject struct {
 	Tarball string `json:"tarball,omitempty"`
 	ShaSum  string `json:"shasum,omitempty"`
 }
 
-// func (o *DistObject) MarshalJSON() ([]byte, error) {
-// 	b := bytes.NewBuffer(nil)
-// 	b.Grow(len(o.Tarball) + len(o.ShaSum) + 26)
-// 	b.WriteString(`{"tarball":"`)
-// 	b.WriteString(o.Tarball)
-// 	b.WriteString(`","shasum":"`)
-// 	b.WriteString(o.ShaSum)
-// 	b.WriteString(`"}`)
-// 	return b.Bytes(), nil
-// }
+func writeStringMap(b *bytes.Buffer, m map[string]string) {
+	first := true
+	for k, v := range m {
+		if !first {
+			b.WriteString(",")
+		}
+		first = false
+		b.WriteString(`"`)
+		b.WriteString(k)
+		b.WriteString(`":"`)
+		b.WriteString(v)
+		b.WriteString(`"`)
+	}
+}
+
+func (a *AbbreviatedMetadata) MarshalJSON() ([]byte, error) {
+	b := bufferPool.Get().(*bytes.Buffer)
+	b.Reset()
+	defer bufferPool.Put(b)
+
+	b.WriteString(`{"name":"`)
+	b.WriteString(a.Name)
+	b.WriteString(`","modified":"`)
+	b.WriteString(a.Modified)
+	b.WriteString(`","versions":{`)
+
+	first := true
+	for k, v := range a.Versions {
+		if !first {
+			b.WriteString(",")
+		}
+		first = false
+		b.WriteString(`"`)
+		b.WriteString(k)
+		b.WriteString(`":`)
+		version, err := v.MarshalJSON()
+		if err != nil {
+			return nil, err
+		}
+		b.Write(version)
+	}
+	b.WriteString(`}`)
+
+	if len(a.DistTags) > 0 {
+		b.WriteString(`,"dist-tags":{`)
+		first = true
+		for k, v := range a.DistTags {
+			if !first {
+				b.WriteString(",")
+			}
+			first = false
+			b.WriteString(`"`)
+			b.WriteString(k)
+			b.WriteString(`":"`)
+			b.WriteString(v)
+			b.WriteString(`"`)
+		}
+		b.WriteString(`}`)
+	}
+
+	b.WriteString("}")
+	return b.Bytes(), nil
+}
+
+func (v *VersionObject) MarshalJSON() ([]byte, error) {
+	b := bufferPool.Get().(*bytes.Buffer)
+	b.Reset()
+	defer bufferPool.Put(b)
+
+	b.WriteString(`{"name":"`)
+	b.WriteString(v.Name)
+	b.WriteString(`","version":"`)
+	b.WriteString(v.Version)
+	b.WriteString(`","dist":`)
+	dist, err := v.Dist.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+	b.Write(dist)
+
+	if v.Deprecated != "" {
+		b.WriteString(`,"deprecated":"`)
+		b.WriteString(v.Deprecated)
+		b.WriteString(`"`)
+	}
+	if len(v.Dependencies) > 0 {
+		b.WriteString(`,"dependencies":{`)
+		first := true
+		for k, v := range v.Dependencies {
+			if !first {
+				b.WriteString(",")
+			}
+			first = false
+			b.WriteString(`"`)
+			b.WriteString(k)
+			b.WriteString(`":"`)
+			b.WriteString(v)
+			b.WriteString(`"`)
+		}
+		b.WriteString(`}`)
+	}
+	if len(v.DevDependencies) > 0 {
+		b.WriteString(`,"devDependencies":{`)
+		first := true
+		for k, v := range v.DevDependencies {
+			if !first {
+				b.WriteString(",")
+			}
+			first = false
+			b.WriteString(`"`)
+			b.WriteString(k)
+			b.WriteString(`":"`)
+			b.WriteString(v)
+			b.WriteString(`"`)
+		}
+		b.WriteString(`}`)
+	}
+	if len(v.PeerDependencies) > 0 {
+		b.WriteString(`,"peerDependencies":{`)
+		first := true
+		for k, v := range v.PeerDependencies {
+			if !first {
+				b.WriteString(",")
+			}
+			first = false
+			b.WriteString(`"`)
+			b.WriteString(k)
+			b.WriteString(`":"`)
+			b.WriteString(v)
+			b.WriteString(`"`)
+		}
+		b.WriteString(`}`)
+	}
+	b.WriteString("}")
+	return b.Bytes(), nil
+}
+
+func (o *DistObject) MarshalJSON() ([]byte, error) {
+	b := bufferPool.Get().(*bytes.Buffer)
+	b.Reset()
+	defer bufferPool.Put(b)
+
+	b.WriteString(`{"tarball":"`)
+	b.WriteString(o.Tarball)
+	b.WriteString(`","shasum":"`)
+	b.WriteString(o.ShaSum)
+	b.WriteString(`"}`)
+	return b.Bytes(), nil
+}
